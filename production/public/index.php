e7186ce301c06cd1038f58ac8209aa9a4fdb1c7a
Michael Houmann <michael@michael-h.dk>
2018-02-04 21:01:39 +0100

    Working on responsive css menu

--- production/public/index.php
+++ production/public/index.php
@@ -12,15 +12,24 @@
 </head>
 <body>
 
-<h1>Test 1</h1>
-<h2>Test 2</h2>
-<h3>Test 3</h3>
-<h4>Test 4</h4>
-<h5>Test 5</h5>
-<h6>Test 6</h6>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiores consequuntur culpa dolorum, ducimus
-    earum illum ipsa iusto labore mollitia nam necessitatibus, neque nihil officia optio pariatur sequi totam.
-    Veniam!</p>
+<header>
+    <div class="logo" role="logo">
+        <a href="forside.php"><img src="./images/nybo_logo.svg" class="logo_head" alt="Theis Nybo Foto logo"></a>
+        <h1 class="noshow">Theis Nybo Fotografi</h1>
+    </div>
+    <div class="menu">
+        <nav id="nav" role="navigation">
+            <input class="menu-btn" type="checkbox" id="menu-btn">
+            <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+            <ul class="menubar">
+                <li><a href="forside.php">Forside</a></li>
+                <li><a href="foto.php">Fotografi</a></li>
+                <li><a href="nyheder.php">Nyheder</a></li>
+                <li><a href="kontakt.php">Kontakt</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
 
 
 <script src="javascript"></script>
