7014cf8a2a5a96c47b70ec6f751625e05b15945c
Michael Houmann <michael@michael-h.dk>
2018-02-24 22:27:47 +0100

    Added empty pages for news section

--- production/public/admin/skriv_nyhed.php
+++ production/public/admin/skriv_nyhed.php
@@ -12,14 +12,6 @@ require_once (SHARED_PATH.'/admin_header.inc.php');
 
 <main>
 
-    <nav class="sidebar_nav">
-
-        <div class="velkommen">
-            <p class="space-under">Velkommen&nbsp;<?php echo h($_SESSION['username']); ?></p>
-        </div>
-
-    </nav>
-
 
 </main>
 
