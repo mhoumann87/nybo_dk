bf049ed5d0cfb1a378fb92fd1021134d9b5be0f7
Michael Houmann <michael@michael-h.dk>
2018-03-11 21:27:39 +0100

    Frontpage and photo page are done wirth css. Frontpage totally done, photopage missing category photos

--- production/public/foto.php
+++ production/public/foto.php
@@ -1,4 +1,4 @@
-<<?php
+<?php
 $title = "Theis Nybo Foto - Fotografi";
 $side = 'foto';
 require_once ('./../private/initialize.inc.php');
@@ -6,6 +6,70 @@ require_once('./../private/shared/header.inc.php');
 ?>
 
 <main>
+
+    <section class="foto-side">
+
+    <aside class="sidebar">
+
+        <?php
+
+        if(isset($_POST['id'])) {
+
+
+
+        } else {
+
+            echo '<nav>';
+            echo '<div class="side-nav">';
+            echo '<div class="side_nav_item"><a class="active" href="' . url_for('/foto.php/') . '">Alle Billeder</div>';
+
+            $kategori = find_all_categories();
+
+            while ($cat = mysqli_fetch_assoc($kategori)) {
+
+                $sql = "SELECT * FROM billeder WHERE kategori_id = '" . $cat['kategori_id'] . "'";
+                $result = mysqli_query($db, $sql);
+                if (mysqli_num_rows($result) != '0') {
+
+                    echo '<div class="side_nav_item"><a href="' . url_for('/foto.php?id=/') . '' . $cat['kategori_id'] . '">' . h($cat['kategori_navn']) . '</a> </div>';
+
+                }
+            }
+
+            echo '</div>';
+            echo '</nav>';
+
+    echo '</aside>';
+
+    echo '<section class="fotos">';
+
+$billeder = find_all_photos();
+
+while($billede = mysqli_fetch_assoc($billeder)) {
+
+    if($billede['billede_width'] > $billede['billede_height']) {
+        $photo_class = 'lands';
+    } else {
+        $photo_class = 'port';
+    }
+
+    echo '<article class="fotoside_fotobox">';
+
+    echo '<div class="foto_fill"><img src="'.$billede['billede_link'].'" class="'.$photo_class.'" alt="'.$billede['billede_titel'].'"></div>';
+    echo '<div class="foto_caption"><h3>'.$billede['billede_titel'].'</h3></div>';
+
+    echo '</article>';
+}
+
+
+
+   echo '</section>';
+
+        }
+        ?>
+
+    </section>
+
 
 
 </main>
--- production/public/index.php
+++ production/public/index.php
@@ -7,111 +7,115 @@ require_once('./../private/shared/header.inc.php');
 
 <main>
 
-    <section class="front-foto">
+    <section class="forside">
 
-        <?php
+        <section class="front-foto">
 
-        $sql = "SELECT * FROM billeder ORDER BY billede_upload DESC LIMIT 8";
-        $result = mysqli_query($db, $sql);
-        confirm_result_set($result);
+            <?php
 
-        while($photo = mysqli_fetch_assoc($result)) {
+            $sql = "SELECT * FROM billeder ORDER BY billede_upload DESC LIMIT 8";
+            $result = mysqli_query($db, $sql);
+            confirm_result_set($result);
 
-            $kategori = find_category_by_id(trim(clean_input($db, $photo['kategori_id'])));
-            $cat = $kategori['kategori_navn'];
+            while($photo = mysqli_fetch_assoc($result)) {
 
-            if($photo['billede_width'] < $photo['billede_height']) {
-                $photo_class = 'port';
-            } else {
-                $photo_class = 'lands';
+                $kategori = find_category_by_id(trim(clean_input($db, $photo['kategori_id'])));
+                $cat = $kategori['kategori_navn'];
+
+                if($photo['billede_width'] < $photo['billede_height']) {
+                    $photo_class = 'port';
+                } else {
+                    $photo_class = 'lands';
+                }
+
+
+                ?>
+                <article class="photo-card">
+
+                    <h3 class="space-under"><?php echo h($photo['billede_titel']) ?? '';?></h3>
+                    <img src="<?php echo $photo['billede_link'] ?? '';?>" class="space-under <?php echo $photo_class ?? '';?>" alt="<?php echo $photo['photo_title'] ?? '';?>">
+                    <p>Kategori : <a href="<?php echo url_for('/foto.php?id='.$photo['kategori_id'].'');?>"><?php echo $cat ?? ''; ?></a></p>
+
+                </article>
+
+                <?php
             }
+            ?>
 
+        </section>
 
-        ?>
-        <article class="photo-card">
+        <section class="front-news">
 
-            <h3 class="space-under"><?php echo h($photo['billede_titel']) ?? '';?></h3>
-            <img src="<?php echo $photo['billede_link'] ?? '';?>" class="space-under <?php echo $photo_class ?? '';?>" alt="<?php echo $photo['photo_title'] ?? '';?>">
-            <p>Kategori : <a href="<?php echo url_for('/foto.php?id='.$photo['kategori_id'].'');?>"><?php echo $cat ?? ''; ?></a></p>
+            <?php
 
-        </article>
+            $sql = "SELECT * FROM news ORDER BY news_time DESC LIMIT 4";
+            $result = mysqli_query($db, $sql);
+            confirm_result_set($result);
 
-        <?php
-        }
-        ?>
+            while($news = mysqli_fetch_assoc($result) ) {
 
-    </section>
+                $kategori = mysqli_fetch_assoc(find_news_cat_by_id(trim(clean_input($db, $news['newskat_id']))));
+                $kat = $kategori['newskat_navn'];
 
-    <section class="front-news">
+                $photos = mysqli_fetch_assoc(find_newsImg_by_id(trim(clean_input($db, $news['newsImg_id']))));
+                $photo = $photos['newsImg_link'];
 
-        <?php
+                if($photos['newsImg_width'] < $photos['newsImg_height']) {
+                    $photo_class = 'port';
+                } else {
+                    $photo_class = 'lands';
+                }
+                ?>
 
-        $sql = "SELECT * FROM news ORDER BY news_time DESC LIMIT 4";
-        $result = mysqli_query($db, $sql);
-        confirm_result_set($result);
+                <article class="news-card">
 
-        while($news = mysqli_fetch_assoc($result) ) {
+                    <div class="top">
+                        <div class="front_newsfoto_box">
+                            <img src="<?php echo $photo ?? ''; ?>" class="front_newsfoto <?php echo $photo_class ?? ''; ?>" alt="<?php echo $news['news_overskrift'] ?? ''; ?>">
+                        </div>
 
-            $kategori = mysqli_fetch_assoc(find_news_cat_by_id(trim(clean_input($db, $news['newskat_id']))));
-            $kat = $kategori['newskat_navn'];
+                        <div class="front_newsbox">
+                            <h4 class="headline"><?php echo $news['news_overskrift'] ?? ''; ?></h4>
+                            <div class="news_info">
+                                <h6>Uploaded <?php echo $news['news_dato'] ?? ''; ?></h6>
+                                <h6>Kategori <a href="<?php echo url_for('/nyheder.php?id='.$kategori['newskat_id'].'');?>"><?php echo $kat ?? ''; ?></a></h6>
+                            </div>
+                        </div>
 
-            $photos = mysqli_fetch_assoc(find_newsImg_by_id(trim(clean_input($db, $news['newsImg_id']))));
-            $photo = $photos['newsImg_link'];
-
-            if($photos['newsImg_width'] < $photos['newsImg_height']) {
-                $photo_class = 'port';
-            } else {
-                $photo_class = 'lands';
-            }
-        ?>
-
-            <article class="news-card">
-
-            <div class="top">
-                <div class="front_newsfoto_box">
-                    <img src="<?php echo $photo ?? ''; ?>" class="front_newsfoto <?php echo $photo_class ?? ''; ?>" alt="<?php echo $news['news_overskrift'] ?? ''; ?>">
-                </div>
-
-                <div class="front_newsbox">
-                    <h4 class="headline"><?php echo $news['news_overskrift'] ?? ''; ?></h4>
-                    <div class="news_info">
-                    <h6>Uploaded <?php echo $news['news_dato'] ?? ''; ?></h6>
-                    <h6>Kategori <a href="<?php echo url_for('/nyheder.php?id='.$kategori['newskat_id'].'');?>"><?php echo $kat ?? ''; ?></a></h6>
                     </div>
-                </div>
+                    <div class="front_news_content">
 
+                        <p><?php echo substr( $news['news_text'], 0 , 150); ?>... <a href="<?php echo url_for('/nyheder.php?newsid='.$news['news_id'].'') ;?>">Læs mere</a> </P>
+                    </div>
+
+                </article>
+
+                <?php
+            }
+            mysqli_free_result($result);
+            ?>
+
+
+        </section>
+
+        <aside class="about">
+
+            <div class="af-box">
+                <img src="<?php echo url_for('/images/theis_placeholder.jpg'); ?>" class="about-foto" alt="Theis Nybo">
             </div>
-                <div class="front_news_content">
 
-                    <p><?php echo substr( $news['news_text'], 0 , 150); ?>... <a href="<?php echo url_for('/nyheder.php?newsid='.$news['news_id'].'') ;?>">Læs mere</a> </P>
-                </div>
+            <div class="about-headline">
+                <h4>Theis Nybo</h4>
+            </div>
 
-            </article>
-
-        <?php
-        }
-        mysqli_free_result($result);
-        ?>
-
+            <div class="about-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Ab ipsam iusto laborum, officiis soluta unde voluptas voluptate?
+                    A amet cum, distinctio facilis iusto nemo neque, perspiciatis quaerat, quas quasi unde!</p>
+            </div>
+        </aside>
 
     </section>
-
-    <aside class="about">
-
-        <div class="af-box">
-            <img src="<?php echo url_for('/images/theis_placeholder.jpg'); ?>" class="about-foto" alt="Theis Nybo">
-        </div>
-
-        <div class="about-headline">
-            <h4>Theis Nybo</h4>
-        </div>
-
-        <div class="about-content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Ab ipsam iusto laborum, officiis soluta unde voluptas voluptate?
-            A amet cum, distinctio facilis iusto nemo neque, perspiciatis quaerat, quas quasi unde!</p>
-        </div>
-    </aside>
 
 </main>
 
